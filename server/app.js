c94d5d0bf174aa3313b0becd2829775a1e0f93f8
Thomas Domingo <tdomingo90@gmail.com>
2020-12-18 15:48:17 -0800

    establish user post route

--- server/app.js
+++ server/app.js
@@ -17,17 +17,17 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 
-app.get('/', 
+app.get('/',
 (req, res) => {
   res.render('index');
 });
 
-app.get('/create', 
+app.get('/create',
 (req, res) => {
   res.render('index');
 });
 
-app.get('/links', 
+app.get('/links',
 (req, res, next) => {
   models.Links.getAll()
     .then(links => {
@@ -38,7 +38,7 @@ app.get('/links',
     });
 });
 
-app.post('/links', 
+app.post('/links',
 (req, res, next) => {
   var url = req.body.url;
   if (!models.Links.isValidUrl(url)) {
@@ -78,7 +78,16 @@ app.post('/links',
 // Write your authentication routes here
 /************************************************************/
 
-
+app.post('/signup', function (req, res) {
+  models.Users.create(req.body)
+  .then((err)=> {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send();
+    }
+  })
+})
 
 /************************************************************/
 // Handle the code parameter route last - if all other routes fail
