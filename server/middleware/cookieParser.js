86da882272e58c18e9c41eaa46bbdfc559c131eb
Thomas Domingo <tdomingo90@gmail.com>
2020-12-18 21:04:49 -0800

    add cookie parsing functionality

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,21 @@
+const hashUtils = require('../lib/hashUtils');
+
 const parseCookies = (req, res, next) => {
+
+  if (req.headers.cookie) {
+    let cookieArr = req.headers.cookie.split('; ');
+    let cookies = {};
+    cookieArr.forEach((cookie) => {
+      let splitCookie = cookie.split('=');
+      cookies[splitCookie[0]] = splitCookie[1];
+    });
+    req.cookies = cookies;
+  }
+  //this belongs in sessions:
+  //  else {
+  //   req.cookies = {shortlyid: hashUtils.createHash(hashUtils.createRandom32String())}
+  // }
+  next();
 };
 
 module.exports = parseCookies;
